b7c487eb94017dcf9324d553a8e4eb8e86923f29
bar2104y <bar2104y@yandex.ru>
2019-08-12 22:17:56 +0300

    Updates

---
+++ precache-manifest.1f58bf1eec28c68ebe0cfbb65c71fd36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5829ee99c0914bfd24d7e7079d2ce6d",
+    "url": "/sapper/index.html"
+  },
+  {
+    "revision": "03210a77e90aa6bf5022",
+    "url": "/sapper/static/css/2.da1bec0f.chunk.css"
+  },
+  {
+    "revision": "528a2726e0b968df174c",
+    "url": "/sapper/static/css/main.8d9e481d.chunk.css"
+  },
+  {
+    "revision": "03210a77e90aa6bf5022",
+    "url": "/sapper/static/js/2.d7babc97.chunk.js"
+  },
+  {
+    "revision": "528a2726e0b968df174c",
+    "url": "/sapper/static/js/main.c501730d.chunk.js"
+  },
+  {
+    "revision": "eebf58037ff792fb60f0",
+    "url": "/sapper/static/js/runtime~main.ea5239f6.js"
+  }
+]);
